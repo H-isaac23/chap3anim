2be5cd031532eb66e84dcb6b1e283a4428b7f234
H-isaac23 <dev.isaac23@gmail.com>
2023-05-07 21:44:15 +0800

    add nn animation

--- presentation.py
+++ presentation.py
@@ -69,9 +69,195 @@ class Archi(Slide):
         Son is the prince
         """
 
-        pre_text = Text("Preprocess the PDF")
-        l1 = Text("The future king is the prince")
-        l2 = Text("Daughter is the princess")
-        l3 = Text("Son is the prince")
+
+class Preprocess(Slide):
+    def play_next(self):
+        indicator = Arrow(buff=0.75)
+        indicator.to_edge(DOWN).to_edge(RIGHT)
+
+        self.play(FadeIn(indicator), run_time=0.75)
+
+        self.wait(1)
+        self.next_slide()
+        self.play(Uncreate(indicator), run_time=0.75)
+
+    def construct(self):
+        self.camera.background_color = "#141414"
+        pre_text = Text("Preprocess the PDF", font_size=24)
+        rem_text = Text("Remove stop words", font_size=24)
+        low_text = Text("Lowercase all letters", font_size=24)
+        ohe_text = Text("Perform one-hot-encoding", font_size=24)
+        pre_text.to_edge(UP)
+        rem_text.to_edge(DOWN)
+        ohe_text.to_edge(DOWN)
+        low_text.to_edge(DOWN)
+
+        one_hot_encoded = Table(
+            [
+                ["1", "0", "0", "0", "0", "0"],
+                ["0", "1", "0", "0", "0", "0"],
+                ["...", "...", "...", "...", "...", "..."],
+                ["0", "0", "0", "0", "0", "1"],
+            ],
+            col_labels=[Text("is_future"), Text("is_king"), Text("is_prince"), Text(
+                "is_daughter"), Text("is_princess"), Text("is_son")],
+            row_labels=[Text("future"), Text("king"), Text("..."), Text("son")]
+        )
+        one_hot_encoded.scale(0.5)
+
+        one_hot_encoded_text = Text(
+            "[[1, 0, 0, 0, 0, 0]\n [1, 0, 0, 0, 0, 0]\n [..., ..., ..., ..., ..., ...]\n [1, 0, 0, 0, 0, 0]]",
+            font_size=24
+        )
+
+        l1 = Text("The future king is the prince", font_size=24)
+        l2 = Text("Daughter is the princess", font_size=24)
+        l3 = Text("Son is the prince", font_size=24)
+
+        rl1 = Text("future king prince", font_size=24)
+        rl2 = Text("Daughter princess", font_size=24)
+        rl3 = Text("Son prince", font_size=24)
+
+        ll2 = Text("daughter princess", font_size=24)
+        ll3 = Text("son prince", font_size=24)
+
+        to_table = VGroup(ll2, ll3, rl1)
+
+        l1.next_to(l2, UP)
+        l3.next_to(l2, DOWN)
+
+        rl1.next_to(l2, UP)
+        rl3.next_to(l2, DOWN)
+
+        ll3.next_to(l2, DOWN)
+
+        # \\\\\\\\\\\\\\\\\ NN
+        c1, c2, c3, c4, c5, c6 = Circle(radius=0.25, color=WHITE, stroke_width=1.25), Circle(
+            radius=0.25, color=WHITE, stroke_width=1.25), Circle(radius=0.25, color=WHITE, stroke_width=1.25), Circle(radius=0.25, color=WHITE, stroke_width=1.25), Circle(
+            radius=0.25, color=WHITE, stroke_width=1.25), Circle(radius=0.25, color=WHITE, stroke_width=1.25)
+        c3.shift(UP*0.5)
+        c4.next_to(c3, 2*DOWN)
+        c5.next_to(c4, 2*DOWN)
+        c6.next_to(c5, 2*DOWN)
+        c2.next_to(c3, 2*UP)
+        c1.next_to(c2, 2*UP)
+
+        c7, c8 = Circle(radius=0.25, color=WHITE, stroke_width=1.25), Circle(
+            radius=0.25, color=WHITE, stroke_width=1.25)
+        c7.shift(0.5*UP)
+        c8.shift(0.5*DOWN)
+
+        c9, c10, c11, c12, c13, c14 = Circle(radius=0.25, color=WHITE, stroke_width=1.25), Circle(
+            radius=0.25, color=WHITE, stroke_width=1.25), Circle(radius=0.25, color=WHITE, stroke_width=1.25), Circle(radius=0.25, color=WHITE, stroke_width=1.25), Circle(
+            radius=0.25, color=WHITE, stroke_width=1.25), Circle(radius=0.25, color=WHITE, stroke_width=1.25)
+        c11.shift(UP*0.5)
+        c12.next_to(c11, 2*DOWN)
+        c13.next_to(c12, 2*DOWN)
+        c14.next_to(c13, 2*DOWN)
+        c10.next_to(c11, 2*UP)
+        c9.next_to(c10, 2*UP)
+
+        layer1 = VGroup(c1, c2, c3, c4, c5, c6)
+        layer2 = VGroup(c7, c8)
+        layer3 = VGroup(c9, c10, c11, c12, c13, c14)
+
+        layer1.shift(2.5*LEFT)
+        layer3.shift(2.5*RIGHT)
+
+        lsin = VGroup()
+        ls1 = VGroup()
+        ls2 = VGroup()
+
+        for n in layer1:
+            line_start = n.get_edge_center(RIGHT)
+            line_ends = []
+            for n_j in layer2:
+                line_ends.append(n_j.get_edge_center(LEFT))
+
+            for end in line_ends:
+                l = Line(line_start, end, stroke_width=0.75, color=GRAY)
+                l2 = Line(end, line_start, stroke_width=0.75, color=GRAY)
+                ls1.add(l)
+
+        for n in layer2:
+            line_start = n.get_edge_center(RIGHT)
+            line_ends = []
+            for n_j in layer3:
+                line_ends.append(n_j.get_edge_center(LEFT))
+
+            for end in line_ends:
+                l = Line(line_start, end, stroke_width=0.75, color=GRAY)
+                l2 = Line(end, line_start, stroke_width=0.75, color=GRAY)
+                ls2.add(l)
+
+        input_dot = Dot()
+        # \\\\\\\\\\\\\\\\\\\\\\\\ End NN
+
+        # Animation Part 2
+
+        # self.play(one_hot_encoded_text.animate.next_to(layer1, LEFT*3))
+        # input_dot.move_to(one_hot_encoded_text.get_center())
+
+        # for n in layer1:
+        #     start = input_dot.get_center()
+        #     end = n.get_edge_center(LEFT)
+        #     l = Line(start, end, stroke_width=0.75, color=GRAY)
+        #     lsin.add(l)
+
+        # self.play(Create(layer1), Create(layer2),
+        #           Create(layer3), Create(ls1), Create(ls2))
+        # self.play(one_hot_encoded_text.animate.become(input_dot))
+        # self.play(ShowPassingFlash(lsin.copy().set_color(
+        #     BLUE), run_time=0.5, time_width=0.5), Uncreate(one_hot_encoded_text), run_time=0.5)
+        # self.play(layer1.animate.set_fill(
+        #     WHITE, opacity=0.5), run_time=0.25)
+        # self.play(layer1.animate.set_fill(
+        #     WHITE, opacity=0), run_time=0.25)
+        # self.play(ShowPassingFlash(ls1.copy().set_color(
+        #     BLUE), run_time=0.5, time_width=0.5))
+        # self.play(layer2.animate.set_fill(
+        #     WHITE, opacity=0.5), run_time=0.25)
+        # self.play(layer2.animate.set_fill(
+        #     WHITE, opacity=0), run_time=0.25)
+        # self.play(ShowPassingFlash(ls2.copy().set_color(
+        #     BLUE), run_time=0.5, time_width=0.5))
+        # self.play(layer3.animate.set_fill(
+        #     WHITE, opacity=0.5), run_time=0.25)
+        # self.play(layer3.animate.set_fill(
+        #     WHITE, opacity=0), run_time=0.25)
+
+        # self.add(l1, l2, l3, pre_text, rem_text)
+        # Animation Part 1
+        # self.play(Create(pre_text))
+        # self.play_next()
+
+        # self.play(Create(l1), Create(l2), Create(l3))
+        # self.play_next()
+
+        # self.play(Create(rem_text), run_time=0.5)
+        # self.play(
+        #     l1.animate.become(rl1),
+        #     l2.animate.become(rl2),
+        #     l3.animate.become(rl3)
+        # )
+        # self.play_next()
+
+        # self.play(Uncreate(rem_text), run_time=0.5)
+        # self.play(Create(low_text), run_time=0.5)
+        # self.play(l2.animate.become(ll2), l3.animate.become(ll3))
+        # self.remove(l1, l2, l3)
+        # self.add(to_table)
+        # self.play_next()
+
+        # self.play(Uncreate(low_text))
+        # self.play(Create(ohe_text))
+        # self.play(to_table.animate.become(one_hot_encoded))
+        # self.play_next()
+
+        # self.play(Uncreate(to_table), Create(one_hot_encoded_text))
+        # self.play_next()
+
+        # one_hot_encoded_text.next_to(layer1, LEFT*3)
+        # self.add(one_hot_encoded_text, layer1, layer2, layer3, ls1, ls2)
 
         # self.wait(3)
